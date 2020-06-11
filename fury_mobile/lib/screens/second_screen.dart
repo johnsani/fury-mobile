61eb411cc7a5acbcc82291afb5c2ae0e1b859a97
maroafenogho <maroafenogho@users.noreply.github.com>
2020-06-11 15:26:32 +0100

    new views

--- fury_mobile/lib/screens/second_screen.dart
+++ fury_mobile/lib/screens/second_screen.dart
@@ -1,8 +1,6 @@
 import 'package:audioplayers/audio_cache.dart';
 import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import '../logic.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SecondScreen extends StatefulWidget {
@@ -52,11 +50,51 @@ class SecondScreenState extends State<SecondScreen> {
                 final player = AudioCache();
                 player.play('note1.wav');
                 widget.scheduleNotification(
-                    interval: int.parse( prefs.getString('interval')),
+                    interval: int.parse(prefs.getString('interval')),
                     title: 'Handwash Reminder',
-                    body: "${prefs.getString('name')} it's time to wash your hands");
+                    body:
+                        "${prefs.getString('name')} it's time to wash your hands");
+
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => SecondRoute(),
+                  ),
+                );
               }),
         ),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      type: MaterialType.transparency,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            child: Image.asset('images/Splash Screen.png'),
+          ),
+          Container(
+            padding: EdgeInsets.all(15.0),
+            alignment: Alignment.bottomCenter,
+            margin: EdgeInsets.only(bottom: 30.0),
+            child: Text(
+              'Well done! \n Your hands must be squeaky clean',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 24.0,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
